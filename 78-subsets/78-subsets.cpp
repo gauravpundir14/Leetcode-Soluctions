610abde7a5f9c76aa75db4d226f61bea2707654a
Gaurav Pundir <75431744+gauravpundir14@users.noreply.github.com>
2022-09-23 19:38:33 +0530

    Time: 3 ms (75.51%), Space: 16.9 MB (8.75%) - LeetHub

---
+++ 78-subsets/78-subsets.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    void   recursion( vector<vector<int>> & ans ,vector<int> nums,vector<int> temp, int i)
+    {
+        if(i==nums.size())
+        {
+            ans.push_back(temp);
+            return  ;
+        }
+        
+        recursion(ans,nums,temp,i+1);
+        temp.push_back(nums[i]);
+        recursion(ans,nums,temp,i+1);
+        
+    }
+    vector<vector<int>> subsets(vector<int>& nums)
+    {
+         vector<vector<int>> ans;
+         vector<int> temp;
+        recursion(ans,nums,temp,0);
+        return ans;
+    }
+};
