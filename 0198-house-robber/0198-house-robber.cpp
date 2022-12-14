46eaf1f6dfa2ce345ecd691a2b494ee72053f2cc
Gaurav Pundir <75431744+gauravpundir14@users.noreply.github.com>
2022-12-14 19:53:05 +0530

    Time: 0 ms (100.00%), Space: 7.9 MB (25.79%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,28 +1,14 @@
 class Solution {
 public:
-
-int helper(vector<int>& nums, int i,int n, vector<int>& dp){
-
-    if(i >= n){
-        return 0;
-    }
-
-    if(dp[i] != -1){
-        return dp[i];
-    }
-
-    int m1 = nums[i];
-    int m2 = helper(nums,i+1,n,dp);
-    int m3 = nums[i] + helper(nums,i+2,n,dp);
-
-    return dp[i] = (m1 > m2) ? (m1 > m3)? m1:m3 : (m2>m3)? m2:m3;
-}
-
-int rob(vector<int>& nums) {
-    
-    int n = nums.size();
-    vector<int> dp(n,-1);
-
-    return helper(nums,0,n,dp);
-}
+	int rob(vector<int>& nums) {
+		vector<int>dp(nums.size());
+		dp[0]=nums[0];
+		for(int i=1;i<nums.size();i++){
+			int pick=nums[i];
+			if(i!=1)pick+=dp[i-2];
+			int notpick=dp[i-1];
+			dp[i]=max(pick,notpick);
+		}
+		return dp[nums.size()-1];
+	}
 };
