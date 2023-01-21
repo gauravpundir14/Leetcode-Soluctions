c78592178d3b119e761ff3efdbf303e00d9564b6
Gaurav Pundir <75431744+gauravpundir14@users.noreply.github.com>
2023-01-21 11:39:50 +0530

    Time: 15 ms (7.36%), Space: 9.7 MB (5.01%) - LeetHub

---
+++ 0093-restore-ip-addresses/0093-restore-ip-addresses.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    bool isValidNumber(string s){
+        return stoi(s)>=0 and stoi(s)<=255 and to_string(stoi(s))==s;
+    }
+    bool isValidIPAddress(string s){
+        int n=s.size();
+        s.push_back('.');
+        int i=0;
+        while(i<=n){
+            string temp;
+            while(s[i]!='.'){
+                temp.push_back(s[i++]);
+            }
+            i++;
+            if(temp.size()>3 or !isValidNumber(temp)){
+                return false;
+            }
+        }
+        s.pop_back();
+        return true;
+    }
+    vector<string> ans;
+    void solve(int idx,int dot,string& ds,string& s){
+        int n=s.size();
+        if(idx==n){
+            if(isValidIPAddress(ds) and dot==0){
+                ans.push_back(ds);
+            }
+            return ;
+        }
+        ds.push_back(s[idx]);
+        if(dot>=1 and ds.back()!='.' and idx!=n-1){
+            ds.push_back('.');
+            solve(idx+1,dot-1,ds,s);
+            ds.pop_back();
+        }
+        solve(idx+1,dot,ds,s);
+        ds.pop_back();
+    }
+    vector<string> restoreIpAddresses(string s) {
+        string ds="";
+        solve(0,3,ds,s);
+        return ans;
+    }
+};
