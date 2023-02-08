76dc83784bc00127b9f1ae6522c6e1da714ba77f
Gaurav Pundir <75431744+gauravpundir14@users.noreply.github.com>
2023-02-08 22:45:04 +0530

    Time: 11 ms (96.89%), Space: 10.4 MB (20.82%) - LeetHub

--- 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.cpp
+++ 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.cpp
@@ -1,20 +1,26 @@
 class Solution {
 public:
-    int minFallingPathSum(vector<vector<int>>& matrix) {
-        int rows = matrix.size();
-        int cols = matrix.size();
-        for(int i = 1; i < rows; i++){
-            for(int j = 0; j < cols; j++){
-                int rowUp = matrix[i-1][j];
-                int leftUp = j>0?matrix[i-1][j-1]:INT_MAX;
-                int rightUp = j<cols-1?matrix[i-1][j+1]:INT_MAX;
-                matrix[i][j] += min(rowUp, min(leftUp, rightUp));
-            }
-        }
-        int answer = INT_MAX;
-        for(int j=0;j < cols; j++){
-            answer = min(answer, matrix[rows-1][j]);
-        }
-        return answer;
-    }
+  
+int f(int i, int j, vector<vector<int>> &mat, vector<vector<int>> &dp)
+{
+    if(j<0 || j>=mat[0].size()) return 1e8;
+    if(i==0) return mat[0][j];
+    if(dp[i][j] != -1) return dp[i][j];
+
+    int u=mat[i][j] + f(i-1, j, mat, dp);
+    int ld=mat[i][j] + f(i-1, j-1, mat, dp);
+    int rd=mat[i][j] + f(i-1, j+1, mat, dp);
+
+    return dp[i][j] = min(u, min(ld, rd));
+}
+
+int minFallingPathSum(vector<vector<int>> &mat)
+{
+    int m=mat.size(), n=mat[0].size();
+    vector<vector<int>> dp(m,vector<int>(n, -1));
+    int mini=1e8;
+    for(int j=0; j<n; j++)
+        mini = min(mini, f(m-1, j, mat, dp));
+    return mini;
+}
 };
