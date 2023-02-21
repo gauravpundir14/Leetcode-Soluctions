4716e586a935e5d87e0ab98c99556e2faf148efa
Gaurav Pundir <75431744+gauravpundir14@users.noreply.github.com>
2023-02-21 20:56:00 +0530

    Time: 35 ms (28.34%), Space: 22.5 MB (16.37%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -1,19 +1,33 @@
 class Solution {
 public:
     int singleNonDuplicate(vector<int>& nums) {
-        int ans=0;
-        unordered_map<int,int>mp;
-        for(int i:nums)
-        {
-            mp[i]++;
+        int start=0;
+        int end=nums.size()-1;
+        int mid;
+        if(end==0){
+            return nums[0];   
+        }else if( nums[0]!=nums[1]){  /*checking if first or last pairs may be our answer or not .
+        if pair having different value then this is our target.*/
+            return nums[0];
+        }else if(nums[end]!=nums[end-1]){
+            return nums[end];
         }
-        for(auto i:mp)
-        {
-            if(i.second!=2)
-            {
-                ans=i.first;
-            }
+
+        // binary search starts from here 
+        while(start<=end){
+         mid=start+(end-start)/2;
+         //base condition when it is unique .see first iamge above 
+         if(nums[mid]!=nums[mid-1] && nums[mid]!=nums[mid+1]){
+             return nums[mid];
+         }
+         //checking for step 5 .see step 5 from above 
+        else if(((mid%2)==0 && nums[mid]==nums[mid+1]) || ((mid%2)==1 && nums[mid]==nums[mid-1])){
+             start=mid+1;
+         }
+        else{        //step 6
+             end=mid-1;
         }
-        return ans;
+        }
+        return -1;
     }
 };
