4051bceb22a48b8960f8ea4c91f250757b4d18a4
Gaurav Pundir <75431744+gauravpundir14@users.noreply.github.com>
2023-07-10 23:08:22 +0530

    Time: 3 ms (36.30%), Space: 5.9 MB (91.79%) - LeetHub

---
+++ 0461-hamming-distance/0461-hamming-distance.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int hammingDistance(int x, int y) {
+        int ans=0;
+    
+        for(int i=0;i<31;i++)
+        {
+          
+            if((x & (1<<i)) != (y & (1<<i)) )
+               ans++;
+        }
+        return ans;
+    }
+};
