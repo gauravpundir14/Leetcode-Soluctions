0bd96b2f155175d06f7c1fae3235868ec4e2ec89
Gaurav Pundir <75431744+gauravpundir14@users.noreply.github.com>
2023-12-02 12:12:46 +0530

    Time: 107 ms (73.63%), Space: 24.4 MB (69.63%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -12,9 +12,7 @@ public:
         sort(nums.begin(), nums.end());
         
         for (int i = 0; i < n - 2; i++) {
-            if (nums[i] > 0) {
-                break;
-            }
+            
             if (i > 0 && nums[i - 1] == nums[i]) {
                 continue;
             }
