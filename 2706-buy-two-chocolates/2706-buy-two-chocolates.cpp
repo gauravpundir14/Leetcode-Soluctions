de012afad794ccce39491cb5114520e00ccd92e1
Gaurav Pundir <75431744+gauravpundir14@users.noreply.github.com>
2023-12-20 07:22:40 +0530

    Time: 11 ms (73.75%), Space: 46.9 MB (58.30%) - LeetHub

---
+++ 2706-buy-two-chocolates/2706-buy-two-chocolates.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int buyChoco(vector<int>& prices, int money) {
+        sort(prices.begin(),prices.end());
+        int first=prices[0];
+        int second=prices[1];
+        
+        if(first+second>money) return money;
+        else
+            return (money-(first+second));
+        
+    }
+};
