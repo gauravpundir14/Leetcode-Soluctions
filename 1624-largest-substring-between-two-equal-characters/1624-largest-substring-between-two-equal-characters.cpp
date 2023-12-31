fa152039f02e450dd68e0041e38cec156448f986
Gaurav Pundir <75431744+gauravpundir14@users.noreply.github.com>
2023-12-31 11:24:39 +0530

    Time: 0 ms (100.00%), Space: 6.8 MB (41.15%) - LeetHub

---
+++ 1624-largest-substring-between-two-equal-characters/1624-largest-substring-between-two-equal-characters.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int maxLengthBetweenEqualCharacters(string s) {
+        int n=s.length();
+        int maxi=-1;
+        for(int i=0;i<n;i++)
+        {
+            for(int j=i+1;j<n;j++)
+            {
+                if(s[i]==s[j])
+                {
+                    if(j-(i+1)>maxi)
+                    {
+                        maxi=j-(i+1);
+                    }
+                }
+            }
+        }
+        return maxi;
+        
+    }
+};
